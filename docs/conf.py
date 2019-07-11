22dbc0210a6490d9c1a551df62aecb3c8a2a0114
gggliuye <gggliuye@gmail.com>
2019-07-11 14:54:59 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -74,6 +74,8 @@ pygments_style = None
 
 # -- Options for HTML output -------------------------------------------------
 
+html_logo = "images/logo.PNG"
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
