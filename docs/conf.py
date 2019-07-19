540423b9efbcade3602c1b0cb57dbfa7e1d2d1d8
gggliuye <gggliuye@gmail.com>
2019-07-19 16:15:01 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,8 @@ html_logo = "images/logo.PNG"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = 'edx-sphinx-theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
