2638afdd379fbc980a76de7c7065be81e69fc06d
gggliuye <gggliuye@gmail.com>
2020-08-17 15:39:34 +0800

    Add files via upload

---
+++ MachineLearningWithGraph/HWs/q4_starter_code/run.sh
@@ -0,0 +1,3 @@
+/home/viki/UTOPA/virtualenv/utopa/bin/python train.py --dataset=cora --model_type=GAT --dropout=0.5 --weight_decay=5e-3 --num_layers=2 --epochs=800
+/home/viki/UTOPA/virtualenv/utopa/bin/python train.py --dataset=enzymes --model_type=GAT --dropout=0.5 --weight_decay=5e-3 --num_layers=2 --epochs=800
+
